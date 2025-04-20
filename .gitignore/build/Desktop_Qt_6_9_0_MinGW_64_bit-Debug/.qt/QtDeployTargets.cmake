87ac5d8c03342e62f3ffb56cebfcd4eded1a73fe
Kshitij Bhaskar <kshitijbhaskar22@gmail.com>
2025-04-20 16:25:31 +0530

    Untrack accidental .gitignore directory

--- .gitignore/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_BTP_GUI_FILE C:/Users/kshit/Documents/BTP GUI/BTP_GUI/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/BTP_GUI.exe)
-set(__QT_DEPLOY_TARGET_BTP_GUI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_BTP_GUI_RUNTIME_DLLS C:/Users/kshit/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Users/kshit/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Users/kshit/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
